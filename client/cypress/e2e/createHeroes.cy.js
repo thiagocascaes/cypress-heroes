af68a308ecb64bd6a14660a6586e6a174a436a4d
Thiago <thiagocascaes@outlook.com.br>
2026-04-24 13:00:26 -0300

    createPage added

--- client/cypress/e2e/createHeroes.cy.js
+++ client/cypress/e2e/createHeroes.cy.js
@@ -1,16 +1,14 @@
 import CreatePage from "../pages/createPage"
 import user from '../fixtures/user.json'
-import LoginPage from '../pages/loginPage'
 
 const createPage = new CreatePage()
-const loginPage = new LoginPage()
 
 describe('Heroes Page', () => {
 
 
     it('Create a new hero', () => {
-        loginPage.accessLoginPage()
-        loginPage.accessWithLogin(user.validUser.username, user.validUser.password)
+        createPage.accessLoginPage()
+        createPage.accessWithLogin(user.validUser.username, user.validUser.password)
         createPage.createNewHero(user.createHero.username)
         createPage.createNewHero(user.createHero.priceNumber)
         createPage.createNewHero(user.createHero.fansNumber)
--- client/cypress/pages/createPage.js
+++ client/cypress/pages/createPage.js
@@ -3,11 +3,15 @@ class CreatePage {
      selectorsList() {
         const selectors = {
             createButton: "[href='/heroes/new']",
-            usernameField: "[name='name']",
+            username: "[name='name']",
             priceNumber: "[name='price']",
             fansNumber: "[name='fans']",
             savesNumber: "[name='saves']",
-            powersType: "[value='9']"
+            powersType: "[value='9']",
+            button: "button",
+            usernameField: "[name='email']",
+            passwordField: "[type='password']",
+            
         }
 
         return selectors
@@ -15,16 +19,25 @@ class CreatePage {
     
     accessLoginPage() {
         cy.visit('http://localhost:3000/heroes')
-        cy.get("button").eq(0).click()
+        cy.get("button").eq(0).click()  
     }
 
-    createNewHero(username) {
-        cy.get(this.selectorsList().createButton).click()
+    accessWithLogin(username, password) {
         cy.get(this.selectorsList().usernameField).type(username)
+        cy.get(this.selectorsList().passwordField).type(password)
+        cy.get(this.selectorsList().button).eq(17).click()
+
+    }
+
+    createNewHero() {
+        cy.get(this.selectorsList().createButton).click()
+        cy.get(this.selectorsList().username).type('Thiago')
         cy.get(this.selectorsList().priceNumber).type('99')
         cy.get(this.selectorsList().fansNumber).type('95')
         cy.get(this.selectorsList().savesNumber).type('98')
         cy.get(this.selectorsList().powersType).click()
+        cy.get('.px-8').contains('button', 'Submit').click();
+
     }
 
 }
